dc3bbad3733bdebb092946384ba6095f9edf4dde
ShashiQubit <87139518+ShashiQubit@users.noreply.github.com>
2025-09-06 17:39:40 +0530

    Update ScriptTest.sh

--- ScriptTest.sh
+++ ScriptTest.sh
@@ -3,11 +3,9 @@
 #SBATCH --ntask-per-node=20
 #SBATCH --time=01:00:00
 #SBATCH --partition=cpu
-#SBATCH --mem=16GB 
 #SBATCH --error=%J.err
 #SBATCH --output=%J.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shashi.kumar@iitgn.ac.in
 cd $SLURM_SUBMIT_DIR
-pip install -r requirements.txt
-python3 test_classical_gan_s&p500.py
+python3 test_cgan.py
